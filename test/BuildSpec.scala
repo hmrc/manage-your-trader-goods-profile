23b6c4f0f92594c3f62f382441cb32643cb0bc76
shanthiOC <shanthi.raghunathan@opencastsoftware.com>
2024-01-02 16:49:10 +0000

    Update BuildSpec.scala to try a fix for failing test case

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -14,14 +14,13 @@
  * limitations under the License.
  */
 
-package test
-
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
 import scala.language.postfixOps
+
 import scala.sys.process._
 
-import org.scalatest.{Matchers, WordSpec}
-
-class BuildSpec extends WordSpec with Matchers {
+class BuildSpec extends AnyWordSpecLike with Matchers {
   "Building the content" should {
     "produce static files" in {
       val result = "bundle install" #&& Process("bundle exec middleman build --build-dir=public/ --clean", None) !
